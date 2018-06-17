919ac099218342ef7cec740a54c4a7a02ad10726
sumaya <sumaya.shahoyi@gmail.com>
2018-06-17 22:37:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 function shout(string){
-  return
-  string.toUpperCase()
+  return string.toUpperCase()
 }
 function whisper(string){
-  return
-  string.toLowerCase()
+  return string.toLowerCase()
 }
 function logShout(string){
   console.log('hello')
