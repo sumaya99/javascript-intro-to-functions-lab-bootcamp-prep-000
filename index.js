7708ef4e9b55171ee5f18271cf698b2dd27fbbf3
sumaya <sumaya.shahoyi@gmail.com>
2018-06-17 22:34:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function shout(string){
   return
   string.toUpperCase()
 }
+function shout(string){
+  return
+  string.toUpperCase()
+}
 function logShout(string){
   console.log('hello')
   'hello'.toUpperCase()
